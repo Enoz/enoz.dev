87426f4fd9a17c63bbc0a172c829dfcb0464159f
Enoz <adudek013@gmail.com>
2025-06-27 16:12:09 -0500

    refactor: Add Prettier + Format

--- enozian.com/src/components/Home/Home.tsx
+++ enozian.com/src/components/Home/Home.tsx
@@ -1,14 +1,15 @@
 import './Home.css'
 import pfp from '../../assets/gh-small.png'
 
-
 export default function Home() {
   return (
     <main id="home">
       <div id="content">
         <img id="profile-picture" src={pfp} />
-        <h1>Arek <i>Enoz</i> Dudek</h1>
+        <h1>
+          Arek <i>Enoz</i> Dudek
+        </h1>
       </div>
-    </main >
+    </main>
   )
 }
--- enozian.com/src/main.tsx
+++ enozian.com/src/main.tsx
@@ -8,5 +8,5 @@ createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 )
