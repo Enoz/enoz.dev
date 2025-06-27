ea5cd01c0c37cd7d9776d53d333819807f2795d6
Enoz <adudek013@gmail.com>
2025-06-27 10:03:09 -0500

    feat: Add cleaner deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,12 @@
 #! /bin/bash
+
+# Clean Files
+echo 'Cleaning old files'
+rm -rf ansible/files
+
+# Copy Files
+echo 'Copying ansible files'
+mkdir -p ansible/files
+cp Caddyfile ansible/files/
+
 ansible-playbook -i ./ansible/hosts ./ansible/website.yml
