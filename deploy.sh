b91497f28037a813cefe999b561c0288012a61c0
Enoz <adudek013@gmail.com>
2025-06-27 12:28:58 -0500

    fix: Caddy Install

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-# Clean Files
-echo '######################### Cleaning old files #########################'
-rm -rf ansible/files
-rm -rf enzoian.com/dist
-
 # Build website
 echo '######################### Building enozian.com #########################'
 echo 'Build '
@@ -12,12 +7,6 @@ pushd enozian.com
 npm ci
 npm run build
 popd
-
-# Copy Files
-echo '######################### Copying files to ansible folder #########################'
-echo 'Copying ansible files'
-mkdir -p ansible/files
-cp -r enozian.com/dist ansible/files/enozian.com
 
 # Playbook
 echo '######################### Executing Playbook #########################'
