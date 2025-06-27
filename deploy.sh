6a640e9a1df4b5bfa7979ec29f5fc04f6a5931e7
Enoz <adudek013@gmail.com>
2025-06-27 10:30:17 -0500

    feat: Working deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
 #! /bin/bash
 
 # Clean Files
-echo 'Cleaning old files'
+echo '######################### Cleaning old files #########################'
 rm -rf ansible/files
+rm -rf enzoian.com/dist
+
+# Build website
+echo '######################### Building enozian.com #########################'
+echo 'Build '
+pushd enozian.com
+npm ci
+npm run build
+popd
 
 # Copy Files
+echo '######################### Copying files to ansible folder #########################'
 echo 'Copying ansible files'
 mkdir -p ansible/files
 cp Caddyfile ansible/files/
+cp -r enozian.com/dist ansible/files/enozian.com
 
+# Playbook
+echo '######################### Executing Playbook #########################'
 ansible-playbook -i ./ansible/hosts ./ansible/website.yml
