3509f0a0be4c491d9a47b721e0c5691c39447f68
Enoz <adudek013@gmail.com>
2025-06-27 16:09:56 -0500

    feat: Add react-router

--- enozian.com/src/main.tsx
+++ enozian.com/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './components/Home/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>,
 )
