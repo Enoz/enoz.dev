11b6aac3991faf24cc54320936a6b8906e3e8fce
Enoz <adudek013@gmail.com>
2025-06-28 00:10:46 -0500

    Revert "feat: Navbar"
    This reverts commit f701a9589cf5e3beb1180c8f97fbb71f620f0362.

--- enozian.com/src/components/Home/Home.tsx
+++ enozian.com/src/components/Home/Home.tsx
@@ -1,15 +1,17 @@
 import './Home.css'
+import { Button } from '@mantine/core'
 import pfp from '../../assets/gh-small.png'
 
 export default function Home() {
   return (
-    <div id="home">
+    <main id="home">
       <div id="content">
         <img id="profile-picture" src={pfp} />
         <h1>
           Arek <i>Enoz</i> Dudek
         </h1>
+        <Button fullWidth>Test</Button>
       </div>
-    </div>
+    </main>
   )
 }
