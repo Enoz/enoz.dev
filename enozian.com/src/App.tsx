5fae3dc1b5db23b68f82557b93cc6c398d134458
Enoz <adudek013@gmail.com>
2025-06-28 00:10:46 -0500

    Revert "feat: Main page Mantine'd"
    This reverts commit ad56a05a536ac577279b34903ff207be7fc5dd11.

--- enozian.com/src/App.tsx
+++ enozian.com/src/App.tsx
@@ -1,26 +1,28 @@
 import { NavLink, Route, Routes } from 'react-router'
 import Home from './pages/Home/Home'
-import { AppShell, Avatar, Button, Flex, Group } from '@mantine/core'
+import { AppShell, Text, Avatar, Button, Flex, Group } from '@mantine/core'
 import pfp from './assets/gh-small.png'
 
 export default function App() {
   return (
-    <AppShell header={{ height: 56, collapsed: false }}>
-      <AppShell.Header >
+    <AppShell>
+      <AppShell.Header>
+
         <Group justify="space-between">
           <NavLink to="/">
             <Flex>
-              <Avatar src={pfp} size="md" style={{ margin: "8" }} />
+              <Avatar src={pfp} />
+              <Text size="xl">Enozian</Text>
             </Flex>
           </NavLink>
-          <NavLink to="/chat"><Button>Chat</Button></NavLink>
+          <NavLink to="/test"><Button>Test</Button></NavLink>
         </Group>
       </AppShell.Header>
 
       <AppShell.Main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/chat" element={<div>test</div>} />
+          <Route path="/test" element={<div>test</div>} />
         </Routes>
       </AppShell.Main>
     </AppShell >
