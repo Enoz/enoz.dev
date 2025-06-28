162b8e09a501c9797e99e401f1ab586c1d49d324
Enoz <adudek013@gmail.com>
2025-06-27 20:07:25 -0500

    Revert "feat: Add Mantine"
    This reverts commit e1412b465b1ca4bd40bee89ab56a4ed978ef11ab.

--- enozian.com/src/components/Home/Home.tsx
+++ enozian.com/src/components/Home/Home.tsx
@@ -1,15 +1,14 @@
 import './Home.css'
-// import pfp from '../../assets/gh-small.png'
+import pfp from '../../assets/gh-small.png'
 
 export default function Home() {
   return (
     <main id="home">
       <div id="content">
-        {/* <img id="profile-picture" src={pfp} /> */}
-        {/* <h1> */}
-        {/*   Arek <i>Enoz</i> Dudek */}
-        {/* </h1> */}
-        <h1>Test</h1>
+        <img id="profile-picture" src={pfp} />
+        <h1>
+          Arek <i>Enoz</i> Dudek
+        </h1>
       </div>
     </main>
   )
--- enozian.com/src/main.tsx
+++ enozian.com/src/main.tsx
@@ -1,20 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import Home from './components/Home/Home.tsx'
-import '@mantine/core/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
-
-const theme = createTheme({
-
-})
-
 
 createRoot(document.getElementById('root')!).render(
-  <MantineProvider theme={theme}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  </MantineProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 )
