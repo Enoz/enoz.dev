a3f4f4c1bd374845f5c02c6c1baf1c44f0eb2a11
Enoz <adudek013@gmail.com>
2025-06-28 00:18:47 -0500

    Reapply "feat: Main page Mantine'd"
    This reverts commit 5fae3dc1b5db23b68f82557b93cc6c398d134458.

--- enozian.com/src/pages/Home/Home.tsx
+++ enozian.com/src/pages/Home/Home.tsx
@@ -1,15 +1,22 @@
 import './Home.css'
 import pfp from '../../assets/gh-small.png'
+import { Center, Flex } from '@mantine/core'
 
 export default function Home() {
   return (
     <div id="home">
-      <div id="content">
-        <img id="profile-picture" src={pfp} />
-        <h1>
-          Arek <i>Enoz</i> Dudek
-        </h1>
-      </div>
+      <Center h="100%">
+        <Flex direction="column">
+          <Center>
+            <img id="profile-picture" src={pfp} />
+          </Center>
+          <h1>
+            Arek <i>Enoz</i> Dudek
+          </h1>
+        </Flex>
+      </Center>
+      {/* <div id="content"> */}
+      {/* </div> */}
     </div>
   )
 }
