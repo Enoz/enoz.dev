334f2a74f9bc892e82bbd790bdb5a104d6311491
Enoz <adudek013@gmail.com>
2025-06-29 16:03:49 -0500

    fix: Docker envs

---
+++ chatter-bot/build.sh
@@ -0,0 +1,6 @@
+docker build -t chatter-bot \
+    --build-arg DISCORD_APP_ID=${DISCORD_APP_ID} \
+    --build-arg DISCORD_APP_PUBLICKEY=${DISCORD_APP_PUBLICKEY} \
+    --build-arg DISCORD_APP_TOKEN=${DISCORD_APP_TOKEN} \
+    .
+
