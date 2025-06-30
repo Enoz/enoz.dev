b4338a56bd4ce046d9e7547b273524c2fca1b22d
Enoz <adudek013@gmail.com>
2025-06-29 22:49:05 -0500

    feat: Working deploy

--- chatter-bot/app.js
+++ chatter-bot/app.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/test", async function (req, res) {
+app.get("/test", async function (_, res) {
   res.status(200).send(process.env.DISCORD_APP_ID);
 });
 
