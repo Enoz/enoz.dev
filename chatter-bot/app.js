abfcfd0da9d2d8a8f6602e385b77660ad77122e8
Enoz <adudek013@gmail.com>
2025-06-30 08:04:35 -0500

    refactor: General cleanup

--- chatter-bot/app.js
+++ chatter-bot/app.js
@@ -1,72 +1,29 @@
-import { registerCommands } from "./register.js";
-import { createChannel, getCategory, getChannels } from "./util.js";
 import {
-  verifyKeyMiddleware,
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-} from "discord-interactions";
-import express from "express";
+  registerCommands,
+  createChannel,
+  getCategory,
+  getChannels,
+} from './discord.js'
+import { verifyKeyMiddleware } from 'discord-interactions'
+import { handleInteraction } from './interactions.js'
+import express from 'express'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
 app.post(
-  "/interactions",
+  '/interactions',
   verifyKeyMiddleware(process.env.DISCORD_APP_PUBLICKEY),
-  async function (req, res) {
-    // Interaction id, type and data
-    const { id, type, data } = req.body;
+  handleInteraction
+)
 
-    /**
-     * Handle verification requests
-     */
-    if (type === InteractionType.PING) {
-      return res.send({ type: InteractionResponseType.PONG });
-    }
+app.get('/test123', async (_, res) => {
+  const cg = await getCategory('test')
+  console.log(cg)
+  return res.status(200).json({ nice: '123' })
+})
 
-    /**
-     * Handle slash command requests
-     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-     */
-    if (type === InteractionType.APPLICATION_COMMAND) {
-      const { name } = data;
-
-      // "test" command
-      if (name === "test") {
-        // Send a message into the channel where command was triggered from
-        return res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            flags: InteractionResponseFlags.IS_COMPONENTS_V2,
-            components: [
-              {
-                type: MessageComponentTypes.TEXT_DISPLAY,
-                // Fetches a random emoji to send from a helper function
-                content: `hello world ${id}`,
-              },
-            ],
-          },
-        });
-      }
-
-      console.error(`unknown command: ${name}`);
-      return res.status(400).json({ error: "unknown command" });
-    }
-
-    console.error("unknown interaction type", type);
-    return res.status(400).json({ error: "unknown interaction type" });
-  },
-);
-
-app.get("/test123", async (_, res) => {
-  const cg = await getCategory("test");
-  return res.status(200).json({ nice: "123" });
-});
-
-registerCommands();
-
+registerCommands()
 app.listen(PORT, () => {
-  console.log("Listening on port", PORT);
-});
+  console.log('Listening on port', PORT)
+})
---
+++ chatter-bot/discord.js
@@ -0,0 +1,86 @@
+const GUILD_TEXT = 0
+const GUILD_CATEGORY = 4
+
+export const API_BASE = 'https://discord.com/api/v10'
+export const MY_SERVER_ID = '1388918758801936397'
+export function getDiscordHeaders() {
+  return {
+    Authorization: `Bot ${process.env.DISCORD_APP_TOKEN}`,
+    'Content-Type': 'application/json',
+  }
+}
+
+/** Registers Slash (/) commands with Discord API */
+export async function registerCommands() {
+  const commandRegister = `${API_BASE}/applications/${process.env.DISCORD_APP_ID}/commands`
+  const commands = [
+    {
+      name: 'test',
+      type: 1,
+      description: 'Debug test command',
+    },
+  ]
+
+  for (const cmd of commands) {
+    const options = {
+      method: 'POST',
+      headers: getDiscordHeaders(),
+      body: JSON.stringify(cmd),
+    }
+    await fetch(commandRegister, options)
+  }
+}
+
+/**
+ * Gets the discord Category object with a given name If the category doesn't
+ * exist, it will be created
+ */
+export async function getCategory(name) {
+  const foundItem = await getChannel(name, null, GUILD_CATEGORY)
+  if (foundItem) {
+    return foundItem
+  }
+  return createChannel(name, null, GUILD_CATEGORY)
+}
+
+export async function getChannels() {
+  return fetch(`${API_BASE}/guilds/${MY_SERVER_ID}/channels`, {
+    headers: getDiscordHeaders(),
+  })
+}
+
+/**
+ * Gets the channel object
+ *
+ * @returns Channel object if exists, null if it doesn't
+ */
+export async function getChannel(name, parent, type) {
+  const channels = await fetch(`${API_BASE}/guilds/${MY_SERVER_ID}/channels`, {
+    headers: getDiscordHeaders(),
+  })
+  const channelsJson = await channels.json()
+  return channelsJson.find((channel) => {
+    return (
+      channel.parent_id == parent &&
+      channel.name === name &&
+      channel.type === type
+    )
+  })
+}
+
+/**
+ * Creates a channel. If parent is supplied, the channel will be created under
+ * the parent channel
+ */
+export async function createChannel(name, type, parent) {
+  const res = await fetch(`${API_BASE}/guilds/${MY_SERVER_ID}/channels`, {
+    method: 'POST',
+    headers: getDiscordHeaders(),
+    body: JSON.stringify({
+      name: name,
+      type: type,
+      parent_id: parent,
+    }),
+  })
+  return res.json()
+}
