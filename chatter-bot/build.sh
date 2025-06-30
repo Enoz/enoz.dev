8bc30c0ff09abe2c6f1f88f0fb69ebc1d09779d7
Enoz <adudek013@gmail.com>
2025-06-29 22:11:11 -0500

    feat: Docker export and build

--- chatter-bot/build.sh
+++
@@ -1,8 +0,0 @@
-docker build -t chatter-bot \
-    --build-arg DISCORD_APP_ID=${DISCORD_APP_ID} \
-    --build-arg DISCORD_APP_PUBLICKEY=${DISCORD_APP_PUBLICKEY} \
-    --build-arg DISCORD_APP_TOKEN=${DISCORD_APP_TOKEN} \
-    .
-
-
-
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,39 @@
 #! /bin/bash
 
+if [ -z "${DISCORD_APP_TOKEN}" ]; then
+    echo "Missing DISCORD_APP_TOKEN env"
+    exit 1
+fi
+
+if [ -z "${DISCORD_APP_PUBLICKEY}" ]; then
+    echo "Missing DISCORD_APP_PUBLICKEY env"
+    exit 1
+fi
+
+if [ -z "${DISCORD_APP_ID}" ]; then
+    echo "Missing DISCORD_APP_ID env"
+    exit 1
+fi
+
 # Build website
 echo '######################### Building enoz.dev #########################'
-echo 'Build '
 pushd enoz.dev
 npm ci
 npm run build
 popd
+
+# Build chatter-bot
+echo '######################### Building chatter-bot #########################'
+pushd chatter-bot
+npm ci
+npm run build
+popd
+
+# Export chatter-bot .env
+rm -f ./ansible/roles/chatter-bot/files/secrets.env
+echo "DISCORD_APP_TOKEN=${DISCORD_APP_TOKEN}" >> ./ansible/roles/chatter-bot/files/secrets.env
+echo "DISCORD_APP_PUBLICKEY=${DISCORD_APP_PUBLICKEY}" >> ./ansible/roles/chatter-bot/files/secrets.env
+echo "DISCORD_APP_ID=${DISCORD_APP_ID}" >> ./ansible/roles/chatter-bot/files/secrets.env
 
 # Playbook
 echo '######################### Executing Playbook #########################'
