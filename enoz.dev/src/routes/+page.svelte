6689fbeab463fe092aa2f42a733365a347f3e430
Enoz <adudek013@gmail.com>
2025-07-03 12:14:49 -0500

    feat: Add profile links

--- enoz.dev/src/routes/+page.svelte
+++ enoz.dev/src/routes/+page.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 	import pfp from '$lib/assets/gh-small.png';
 	import githubLogo from './assets/github-mark-white.svg';
+	import discordLogo from './assets/discord-icon-svgrepo-com.svg';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import Profile from './components/profile.svelte';
+
+	const profiles = [
+		{
+			text: 'Github',
+			icon: githubLogo,
+			bgcolor: 'bg-neutral-800/90',
+			url: 'https://github.com/Enoz'
+		},
+		{
+			text: 'Discord',
+			icon: discordLogo,
+			bgcolor: 'bg-[#7289da]',
+			url: 'https://discordapp.com/users/206412058516783104'
+		}
+	];
 </script>
 
 <div class="flex h-full flex-col items-center justify-center">
@@ -14,8 +30,17 @@
 		>
 	</div>
 
-	<div class="pt-[2rem]">
-		<Profile icon={githubLogo} bgcolor="bg-neutral-800/90" url="https://github.com/Enoz" />
+	<div class="flex pt-[2rem]">
+		{#each profiles as profile (profile.text)}
+			<span class="pr-[5px] pl-[5px]">
+				<Profile
+					text={profile.text}
+					icon={profile.icon}
+					bgcolor={profile.bgcolor}
+					url={profile.url}
+				/>
+			</span>
+		{/each}
 	</div>
 </div>
 
--- enoz.dev/src/routes/components/profile.svelte
+++ enoz.dev/src/routes/components/profile.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	const { icon, bgcolor, url } = $props();
+	const { icon, bgcolor, url, text } = $props();
 </script>
 
 <a class="flex rounded-full border-2 border-white p-[7px] {bgcolor}" href={url}>
-	<img class="clip-circle mr-[5px] h-[2rem] w-[2rem]" src={icon} alt="Social" />
-	<p class="text-[1.5rem] font-bold">Github</p>
+	<img class="clip-circle mr-[5px] h-[2rem] w-[2rem] text-white" src={icon} alt="Social" />
+	<p class="text-[1.5rem] font-bold">{text}</p>
 </a>
 
 <style>
