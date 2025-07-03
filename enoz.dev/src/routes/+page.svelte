339bdbeb05f77f6aeb75a7b92d43b6216f83e184
Enoz <adudek013@gmail.com>
2025-07-03 12:36:06 -0500

    fix: Highlighting on main page

--- enoz.dev/src/routes/+page.svelte
+++ enoz.dev/src/routes/+page.svelte
@@ -25,8 +25,8 @@
 	<div class="grid">
 		<img class="clip-circle w-3xs" src={pfp} alt="Github Profile" />
 		<h1 class="mt-6 text-center text-3xl">Arek <i class="text-amber-600">Enoz</i> Dudek</h1>
-		<Button class="mt-3 text-2xl" href="/chat"
-			><span class="h-[1rem] w-[1rem] animate-pulse rounded-full bg-green-500"></span> Live Chat</Button
+		<Button class="mt-3 text-2xl select-none" href="/chat"
+			><span class=" h-[1rem] w-[1rem] animate-pulse rounded-full bg-green-500"></span> Live Chat</Button
 		>
 	</div>
 
--- enoz.dev/src/routes/components/profile.svelte
+++ enoz.dev/src/routes/components/profile.svelte
@@ -2,7 +2,7 @@
 	const { icon, bgcolor, url, text } = $props();
 </script>
 
-<a class="flex rounded-full border-2 border-white p-[7px] {bgcolor}" href={url}>
+<a class="select-none flex rounded-full border-2 border-white p-[7px] {bgcolor}" href={url}>
 	<img class="clip-circle mr-[5px] h-[2rem] w-[2rem] text-white" src={icon} alt="Social" />
 	<p class="text-[1.5rem] font-bold">{text}</p>
 </a>
