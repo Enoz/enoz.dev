9cf2cf7174e5c60de1aab93ad8db4aeaea27a0be
Enoz <adudek013@gmail.com>
2025-07-03 12:02:45 -0500

    feat: Add github link

--- enoz.dev/src/routes/+page.svelte
+++ enoz.dev/src/routes/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import pfp from '$lib/assets/gh-small.png';
+	import githubLogo from './assets/github-mark-white.svg';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import Profile from './components/profile.svelte';
 </script>
 
-<div class="grid h-full items-center justify-center">
+<div class="flex h-full flex-col items-center justify-center">
 	<div class="grid">
 		<img class="clip-circle w-3xs" src={pfp} alt="Github Profile" />
 		<h1 class="mt-6 text-center text-3xl">Arek <i class="text-amber-600">Enoz</i> Dudek</h1>
 		<Button class="mt-3 text-2xl" href="/chat"
 			><span class="h-[1rem] w-[1rem] animate-pulse rounded-full bg-green-500"></span> Live Chat</Button
 		>
+	</div>
+
+	<div class="pt-[2rem]">
+		<Profile icon={githubLogo} bgcolor="bg-neutral-800/90" url="https://github.com/Enoz" />
 	</div>
 </div>
 
---
+++ enoz.dev/src/routes/components/profile.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const { icon, bgcolor, url } = $props();
+</script>
+
+<a class="flex rounded-full border-2 border-white p-[7px] {bgcolor}" href={url}>
+	<img class="clip-circle mr-[5px] h-[2rem] w-[2rem]" src={icon} alt="Social" />
+	<p class="text-[1.5rem] font-bold">Github</p>
+</a>
+
+<style>
+	.clip-circle {
+		clip-path: circle();
+	}
+</style>
