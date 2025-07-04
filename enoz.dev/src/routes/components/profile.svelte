ed774772d19024fa5a5e6f89e87e6c101b9449ab
Enoz <adudek013@gmail.com>
2025-07-03 19:54:15 -0500

    refactor: Remove unused clip-circle class

--- enoz.dev/src/routes/components/profile.svelte
+++ enoz.dev/src/routes/components/profile.svelte
@@ -6,9 +6,3 @@
 	<img class="clip-circle mr-[5px] h-[2rem] w-[2rem] text-white" src={icon} alt="Social" />
 	<p class="text-[1.5rem] font-bold text-white">{text}</p>
 </a>
-
-<style>
-	.clip-circle {
-		clip-path: circle();
-	}
-</style>
