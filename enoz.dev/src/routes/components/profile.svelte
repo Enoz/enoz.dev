0ea534459d042f4aba019b6012e4e706877ce2cd
Enoz <enoz@enoz.dev>
2026-01-12 20:12:44 -0600

    chore: Update dependencies and fix lint issues

--- enoz.dev/src/routes/components/profile.svelte
+++ enoz.dev/src/routes/components/profile.svelte
@@ -2,6 +2,7 @@
 	const { icon, bgcolor, url, text } = $props();
 </script>
 
+<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
 <a class="flex rounded-full border-2 border-white p-[7px] select-none {bgcolor}" href={url}>
 	<img class="clip-circle mr-[5px] h-[2rem] w-[2rem] text-white" src={icon} alt="Social" />
 	<p class="text-[1.5rem] font-bold text-white">{text}</p>
